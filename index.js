8e83c23e08926d87c63d42424372873e7b44eab4
mihon.testosterone <mihon.testosterone@gmail.com>
2021-10-29 23:08:57 +0300

    Works partly

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
-const { Command } = require("commander");
-const program = new Command();
 const {
     listContacts,
     addContact,
     getContactById,
     removeContact,
 } = require("./contacts.js");
+const { Command } = require('commander');
 
+const program = new Command();
+program
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
+try{    switch (action) {
+        case 'list':
+            const contacts=await listContacts()
+            break;
+
+        case 'get':
+            const user=await getContactById(id)
+            if (user) {
+                console.log(user);
+            } else {
+                console.log(chalk.red("Contact not found"));
+            }
+            break;
+
+        case 'add':
+            addContact(name, email, phone)
+            break;
+
+        case 'remove':
+            const contactToRemove = await removeContact(id)
+            break;
+
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+}
+} catch (error) {
+    console.log(error.message);
+}
+}
+
+invokeAction(argv);
 
